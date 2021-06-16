742dfea34a2615eea00ca139754b18187cc62876
Felipe Matheus <felipe_cavalcante7939@hotmail.com>
2021-06-16 15:04:34 -0300

    Adicionando tabela de multiplicação.

---
+++ index.js
@@ -0,0 +1,14 @@
+function multiplicationTable(n) {
+
+    let x = [];
+
+    for (let i = 0; i <= n; i++) {
+        x[i] = [];
+        
+        for (let j = 0; j <= n; j++) {
+            x[i][j] = i * j;
+        }
+    }
+    console.table(x);
+}
+multiplicationTable(10);
